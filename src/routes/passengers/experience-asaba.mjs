3d75ea9b2df2131e3ee9ba3af68ea1c15b843048
areel007 <morenikejisunday@gmail.com>
2024-10-30 10:39:40 -0700

    fix

--- src/routes/passengers/experience-asaba.mjs
+++ src/routes/passengers/experience-asaba.mjs
@@ -2,7 +2,7 @@ import { Router } from "express";
 import {
   getExperienceAsaba,
   newExperienceAsaba,
-} from "../../controllers/passengers/home.mjs";
+} from "../../controllers/passengers/experience-asaba.mjs";
 import { updateAtTheAirport } from "../../controllers/passengers/at-the-airport.mjs";
 
 const router = Router();
